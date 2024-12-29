faecefb4686b9c4df5b474781ab6e9a22de43ade
Shreyansh <135205568+shreyanshchaubey@users.noreply.github.com>
2024-12-29 06:58:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const app = express();
 
 // Database connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'cash_flow_minimizer_v2'
+    host: process.env.DB_HOST 
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
