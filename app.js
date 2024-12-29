6984fe336bf60fef5b023762ce286723b8ea14e3
Shreyansh <135205568+shreyanshchaubey@users.noreply.github.com>
2024-12-29 07:01:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Database connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST 
+    host: process.env.DB_HOST ,
     user: process.env.DB_USER ,
     password: process.env.DB_PASSWORD ,
     database: process.env.DB_NAME
